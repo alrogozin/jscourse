370624c4e95304539fc96b3df5f31cf11ec3e052
Aleksei Rogozin <alrogozin@gmail.com>
2019-05-21 14:26:44 +0300

    210519

--- run.js
+++ run.js
@@ -1,2 +1,16 @@
-console.log('test');
-alert('8881');
+let num = 1;
+while(num < 55) {
+	console.log(num);
+	num++;
+	// break;
+}
+do{
+	console.log(num);
+	num++;
+} while (num < 55)
+
+for(i=0; i < 10; i++) {
+	if(i == 6) break; // прервать выполнение цикла
+	if(i == 3) continue; // пропускаем одну итерацию цикла
+	console.log(':::', i);
+}
--- task_1/run_lesson1.js
+++ task_1/run_lesson1.js
@@ -6,15 +6,48 @@ let appData = {
 	income: [],
 	savings: false
 };
-let money = prompt('Budget?', '100');
+let money = +prompt('Budget?', '100'); // + - способ преобразования к числу (NB!!!)
 let time = prompt('Input date (YYY-MM-DD)', '2019-10-01');
-let mAsk = prompt('Статья затрат (наименование)?');
-let mVal = prompt('Сумма (проект)?');
 
-appData.budget = parseFloat(money);
+// appData.budget = parseFloat(money);
+appData.budget = money;
 appData.timeData = new Date(time);
 
-// Динамическое добавление свойства в объект
-appData.expenses[mAsk] = mVal;
-alert('per Day:' + appData.budget/30);
+for (i=0; i<2; i++) {
+	let mAsk = prompt('Статья затрат (наименование)?');
+	let mVal = prompt('Сумма?');
+	if( 
+		(typeof(mAsk)) === 'string' // Избыточно - prompt возвращает строку
+		&& 
+		(typeof(mAsk) != null) // null - пользователь нажал "отмена"
+		&& 
+		(typeof(mVal) != null) // null - пользователь нажал "отмена"
+		&&
+		mAsk != '' && mVal != '' // Не пустое значение
+		&&
+		mAsk.length < 50 // Ограничение по длине
+	){
+		// Динамическое добавление свойства в объект
+		appData.expenses[mAsk] = mVal;
+	} else {
+		i--;
+	}
+}
+
+appData.moneyPerDay = appData.budget/30;
+alert('per Day:' + appData.moneyPerDay);
 console.log(appData);
+switch(true) { // способ оформить switch для логических значенией в case
+	case (appData.moneyPerDay < 100):
+		console.log('min');
+		break;
+	case (appData.moneyPerDay >= 100 && appData.moneyPerDay < 1000): 
+		console.log('avg');
+		break;
+	case (appData.moneyPerDay >= 1000):
+		console.log('max');
+		break;
+	default:
+		console.log('произошла ошибка:' + appData.moneyPerDay);
+		break;
+}
