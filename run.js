19d87de5978b7abad0cc4a17fc69ddb8b716a406
humster <humster>
2019-05-22 17:31:15 +0300

    22.05.19

--- run.js
+++ run.js
@@ -1,16 +1,27 @@
-function glbFunc() {
-	let i = 0;
-	const f = function(n) {
-		i = i + n;
-		return i;
-	}
-	return f;
+function test_cb(strMessage, cbFunction) {
+	console.log(strMessage);
+	cbFunction();
 }
-let test = glbFunc();
-const c1 = test(1);
-const c2 = test(1);
-const c3 = test(1);
-console.log(':', c1, c2, c3);
+function cbf() {
+	console.log('OOOOps');
+}
+cbf2 = () => console.log('1111');
+test_cb('ываываыв', cbf2);
+
+// NB!!! Closers 
+// function glbFunc() {
+// 	let i = 0;
+// 	const f = function(n) {
+// 		i = i + n;
+// 		return i;
+// 	}
+// 	return f;
+// }
+// let test = glbFunc();
+// const c1 = test(1);
+// const c2 = test(1);
+// const c3 = test(1);
+// console.log(':', c1, c2, c3); // output: ": 1 2 3"
 
 // let num = 1;
 // while(num < 55) {
