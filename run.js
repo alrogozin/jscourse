414e74df301ec6400c5a5b4c9c1f72387c1eb565
Aleksei Rogozin <alrogozin@gmail.com>
2019-05-24 16:04:41 +0300

    240519

--- run.js
+++ run.js
@@ -1,13 +1,44 @@
-function test_cb(strMessage, cbFunction) {
-	console.log(strMessage);
-	cbFunction();
-}
-function cbf() {
-	console.log('OOOOps');
-}
-cbf2 = () => console.log('1111');
-test_cb('ываываыв', cbf2);
+// ------------------------------------------
+// ------------------------------------------
+// ------------------------------------------
+// let baseClass = {
+// 	health: 600,
+// 	armor: 100
+// }
+// let childClass = {
+// 	health: 128
+// }
+// childClass.__proto__ = baseClass;
+// console.log(childClass);
+// console.log(childClass.armor);
 
+function User(name) {
+    this.name = name;
+    var phrase = "Hi";
+    function  getFullName() {
+        return name + "...";
+    } ;
+    this.sayHi = function() {
+        console.log(phrase + getFullName());
+    };
+}
+let gg = new User('test');
+gg.sayHi();
+console.log(gg.phrase);
+
+// ------------------------------------------
+// Пример callback function
+// function test_cb(strMessage, cbFunction) {
+// 	console.log(strMessage);
+// 	cbFunction();
+// }
+// function cbf() {
+// 	console.log('OOOOps');
+// }
+// cbf2 = () => console.log('1111');
+// test_cb('ываываыв', cbf2);
+
+// ------------------------------------------
 // NB!!! Closers 
 // function glbFunc() {
 // 	let i = 0;
@@ -23,6 +54,7 @@ test_cb('ываываыв', cbf2);
 // const c3 = test(1);
 // console.log(':', c1, c2, c3); // output: ": 1 2 3"
 
+// ------------------------------------------
 // let num = 1;
 // while(num < 55) {
 // 	console.log(num);
--- task_2/run_lesson1.js
+++ task_2/run_lesson1.js
@@ -6,7 +6,77 @@ let appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: true
+	savings: true,
+	chooseExpenses: function () {
+		for (i=0; i<2; i++) {
+			let mAsk = prompt('Статья затрат (наименование)?');
+			let mVal = prompt('Сумма?');
+			if( 
+				(typeof(mAsk)) === 'string' // Избыточно - prompt возвращает строку
+				&& 
+				(typeof(mAsk) != null) // null - пользователь нажал "отмена"
+				&& 
+				(typeof(mVal) != null) // null - пользователь нажал "отмена"
+				&&
+				mAsk != '' && mVal != '' // Не пустое значение
+				&&
+				mAsk.length < 50 // Ограничение по длине
+			){
+				// Динамическое добавление свойства в объект
+				appData.expenses[mAsk] = mVal;
+			} else {
+				i--;
+			}
+		}
+	},
+	detectDayBudget: function () {
+		appData.moneyPerDay = (appData.budget/30).toFixed(2);
+		alert('per Day:' + appData.moneyPerDay);
+	},
+	detectLevel: function () {
+		switch(true) { // способ оформить switch для логических значенией в case
+			case (appData.moneyPerDay < 100):
+				console.log('min');
+				break;
+			case (appData.moneyPerDay >= 100 && appData.moneyPerDay < 1000): 
+				console.log('avg');
+				break;
+			case (appData.moneyPerDay >= 1000):
+				console.log('max');
+				break;
+			default:
+				console.log('произошла ошибка:' + appData.moneyPerDay);
+				break;
+		}
+	},
+	checkSavings: function () {
+		if(appData.savings) {
+			let save = +prompt("Сумма накоплений?"),
+					percent = +prompt("Под какой процент?");
+			appData.monthIncome = save/100/12*percent;
+			alert("Доход в месяц:" + appData.monthIncome);
+		}
+	},
+	chooseOptExpenses: function () {
+		for(i=1; i<4; i++) {
+			appData.optionalExpenses[i] = prompt("Статья необязательных расходов");
+		}
+	},
+	chooseIncome: function() {
+		let items;
+		while( items == "" || items == null){
+			items = prompt('что принесет доп.доход? (перечистите чере запятую)', '');
+		}
+		appData.income = items.split(',');
+		appData.income.push(prompt('Может еще что-нибудь?'));
+		appData.income.sort();
+		let strListItems = 'Способы доп.заработка:\n';
+		appData.income.forEach((element, index) => {
+			if(index > 0 ) 
+				strListItems = strListItems + index + " " + element + "\n";
+		});
+		alert(strListItems);
+	}
 };
 
 function start() {
@@ -22,74 +92,21 @@ function start() {
 	// appData.budget = parseFloat(money);
 }
 
-function chooseExpenses() {
-	for (i=0; i<2; i++) {
-		let mAsk = prompt('Статья затрат (наименование)?');
-		let mVal = prompt('Сумма?');
-		if( 
-			(typeof(mAsk)) === 'string' // Избыточно - prompt возвращает строку
-			&& 
-			(typeof(mAsk) != null) // null - пользователь нажал "отмена"
-			&& 
-			(typeof(mVal) != null) // null - пользователь нажал "отмена"
-			&&
-			mAsk != '' && mVal != '' // Не пустое значение
-			&&
-			mAsk.length < 50 // Ограничение по длине
-		){
-			// Динамическое добавление свойства в объект
-			appData.expenses[mAsk] = mVal;
-		} else {
-			i--;
-		}
-	}
-}
-
-function detectDayBudget() {
-	appData.moneyPerDay = (appData.budget/30).toFixed(2);
-	alert('per Day:' + appData.moneyPerDay);
-}
-
-function detectLevel() {
-	switch(true) { // способ оформить switch для логических значенией в case
-		case (appData.moneyPerDay < 100):
-			console.log('min');
-			break;
-		case (appData.moneyPerDay >= 100 && appData.moneyPerDay < 1000): 
-			console.log('avg');
-			break;
-		case (appData.moneyPerDay >= 1000):
-			console.log('max');
-			break;
-		default:
-			console.log('произошла ошибка:' + appData.moneyPerDay);
-			break;
-	}
-}
-
-function checkSavings() {
-	if(appData.savings) {
-		let save = +prompt("Сумма накоплений?"),
-				percent = +prompt("Под какой процент?");
-		appData.monthIncome = save/100/12*percent;
-		alert("Доход в месяц:" + appData.monthIncome);
-	}
-}
-
-function chooseOptExpenses() {
-	for(i=1; i<4; i++) {
-		appData.optionalExpenses[i] = prompt("Статья необязательных расходов");
-	}
-}
-
 // ========================
+/*
 start();
-chooseExpenses();
-detectDayBudget();
-detectLevel();
-checkSavings();
-chooseOptExpenses();
+appData.chooseExpenses();
+appData.detectDayBudget();
+appData.detectLevel();
+appData.checkSavings();
+appData.chooseOptExpenses();
+appData.chooseIncome();
+*/
 // ========================
-
-console.log(appData);
-
+// for(let i in appData) {
+// 	console.log(i, ':', appData[i]);
+// }
+// console.log(appData);
+// ========================
+let box = document.getElementById('box');
+console.log(typeof(box));
